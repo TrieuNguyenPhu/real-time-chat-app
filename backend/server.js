052fce22a9eae7cccef8dea90e5953de109a9e1e
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2026-03-08 08:38:27 +0700

    backend + database setup

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import http from 'http';
+
+import { connectDB } from './utils/db.js';
+
+const app = express();
+const httpServer = http.createServer(app);
+
+app.use(cors({
+    origin: process.env.CLIENT_ORIGIN,
+    credentials: true
+}));
+
+app.use(cookieParser());
+
+try {
+    await connectDB();
+    const PORT = process.env.PORT || 4000;
+    httpServer.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+} catch (error) {
+    console.error('Error starting server:', error);
+}
